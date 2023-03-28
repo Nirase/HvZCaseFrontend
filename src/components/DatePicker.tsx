44471d7daa21731f6904d1f5c7ec7b2e7333016d
Danielle <ninoue.hamrin@gmail.com>
2023-03-28 15:20:47 +0200

    feat: not able to choose to old date or too new

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,13 +1,18 @@
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { Dayjs } from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getOneGame } from "../api/apiCalls";
 
 type Props = {
   sendSelectedDate: (Date: string) => void;
 };
 
 const DatePickerComponent = ({ sendSelectedDate }: Props) => {
+  const { gameId }: any = useParams();
   const [date, setDate] = useState<Dayjs | null>(null);
+
+  const [endDate, setEndDate] = useState<Dayjs>();
 
   useEffect(() => {
     if (date) {
@@ -15,6 +20,16 @@ const DatePickerComponent = ({ sendSelectedDate }: Props) => {
       const stringDate = date.toDate().toLocaleString().split(",", 1)[0];
       sendSelectedDate(stringDate);
     }
+
+    const getTheGame = async () => {
+      const data = await getOneGame(+gameId);
+      if (data) {
+        const date = dayjs(data.endDate);
+        setEndDate(date);
+      }
+    };
+
+    getTheGame();
   }, [date]);
 
   return (
@@ -22,6 +37,8 @@ const DatePickerComponent = ({ sendSelectedDate }: Props) => {
       format="DD/MM/YYYY"
       value={date}
       onChange={(newValue) => setDate(newValue)}
+      disablePast
+      maxDate={endDate}
     />
   );
 };
